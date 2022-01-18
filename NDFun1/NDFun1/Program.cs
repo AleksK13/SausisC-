abcdc7c2bb6ebb867820ca2ac2b6a39b17f35a22
Aleksandr <aleksandr.kulsa@gmail.com>
2022-01-18 11:35:55 +0100

    Fun ND su Console

---
+++ NDFun1/NDFun1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDFun1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite varda:");
+            String vardas = Console.ReadLine();
+
+            Console.WriteLine("Iveskite Amziu:");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Iveskite megstama double skaiciu:");
+            double skaicius = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Tavo vardas yra {vardas}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Tavo amzius yra {amzius}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Tavo skaicius yra {skaicius}");
+
+            Console.ReadLine();
+        }
+    }
+}
